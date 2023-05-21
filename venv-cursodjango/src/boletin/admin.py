078c5234eba8e8621b6573eae086e7536f27252a
DunlapIvan-Clase <ivan.dunlap.iglesias.alu@iesfernandoaguilar.es>
2023-05-21 06:29:53 +0200

    AdminRegistrado personalizado

--- venv-cursodjango/src/boletin/admin.py
+++ venv-cursodjango/src/boletin/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Registrado
 
-admin.site.register(Registrado)
+class AdminRegistrado(admin.ModelAdmin):
+    list_display = ["email", "nombre", "timestamp"]
+    list_filter = ["timestamp"]
+    list_editable = ["nombre"]
+    search_fields = ["email", "nombre"]
+    class meta:
+        model = Registrado
+
+admin.site.register(Registrado, AdminRegistrado)

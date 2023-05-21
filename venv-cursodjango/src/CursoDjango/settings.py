fb6e2e9f74dfd682e8ce978d69c232fe0b9ea136
DunlapIvan-Clase <ivan.dunlap.iglesias.alu@iesfernandoaguilar.es>
2023-05-21 21:16:40 +0200

    Crispy forms

--- venv-cursodjango/src/CursoDjango/settings.py
+++ venv-cursodjango/src/CursoDjango/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boletin'
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'boletin',
+
 ]
 
 MIDDLEWARE = [
@@ -51,6 +54,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'CursoDjango.urls'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
